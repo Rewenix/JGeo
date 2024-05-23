5fa35033ac4c419b72226d7cc99aa1ecd67a5b49
Mateusz Przebieracz <m.przebieracz@student.uj.edu.pl>
2024-05-23 20:46:55 +0200

    BuilderUtils modification

--- src/main/java/Project/controller/builders/BuilderUtils.java
+++ src/main/java/Project/controller/builders/BuilderUtils.java
@@ -6,28 +6,29 @@ import Project.view.*;
 public class BuilderUtils { // Zobaczymy czy jest to przydatne. Najwyzej sie usunie
     public static final double EPSILON = 1e-9; // TODO read it from properties
 
-    public static void addToPlane(GeometricPoint point, ViewablePlane viewablePlane, Plane2D plane) {
-        plane.addGeometricShape(point);
-        viewablePlane.addViewableShape(new ViewablePoint(point));
+    public static void addToPlane(GeometricPoint point, ViewablePlane viewablePlane) {
+        viewablePlane.getPlane().addGeometricShape(point);
+        viewablePlane.addViewableShape(new ViewablePoint("", viewablePlane, point));
     }
 
-    public static void addToPlane(GeometricLine line, ViewablePlane viewablePlane, Plane2D plane) {
-        plane.addGeometricShape(line);
-        viewablePlane.addViewableShape(new ViewableLine(line));
+    public static void addToPlane(GeometricLine line, ViewablePlane viewablePlane) {
+        viewablePlane.getPlane().addGeometricShape(line);
+        viewablePlane.addViewableShape(new ViewableLine("", viewablePlane, line));
     }
 
-    public static void addToPlane(GeometricCircle circle, ViewablePlane viewablePlane, Plane2D plane) {
-        plane.addGeometricShape(circle);
-        viewablePlane.addViewableShape(new ViewableCircle(circle));
+    public static void addToPlane(GeometricCircle circle, ViewablePlane viewablePlane) {
+        viewablePlane.getPlane().addGeometricShape(circle);
+        viewablePlane.addViewableShape(new ViewableCircle("", viewablePlane, circle));
     }
 
-    public static void addToPlane(GeometricSegment segment, ViewablePlane viewablePlane, Plane2D plane) {
-        plane.addGeometricShape(segment);
-        viewablePlane.addViewableShape(new ViewableSegment(segment));
+    public static void addToPlane(GeometricSegment segment, ViewablePlane viewablePlane) {
+        viewablePlane.getPlane().addGeometricShape(segment);
+        viewablePlane.addViewableShape(new ViewableSegment("", viewablePlane, segment));
     }
 
-    public static void addToPlane(GeometricGenCircle genCircle, ViewablePlane viewablePlane, Plane2D plane) {
-        plane.addGeometricShape(genCircle);
-        viewablePlane.addViewableShape(new ViewableGenCircle(genCircle));
+    public static void addToPlane(GeometricGenCircle genCircle, ViewablePlane viewablePlane) {
+        viewablePlane.getPlane().addGeometricShape(genCircle);
+        viewablePlane.addViewableShape(new ViewableCircle("", viewablePlane, genCircle.circle));
+        viewablePlane.addViewableShape(new ViewableLine("", viewablePlane, genCircle.line));
     }
 }
