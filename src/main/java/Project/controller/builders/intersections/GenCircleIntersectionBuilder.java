003b7648b71d16cbb712306daa4668c896e90b6e
lynx9687 <lynx9687@github.com>
2024-05-25 00:21:29 +0200

    Implemented GenCircle intersections

---
+++ src/main/java/Project/controller/builders/intersections/GenCircleIntersectionBuilder.java
@@ -0,0 +1,67 @@
+package Project.controller.builders.intersections;
+
+import Project.controller.builders.BuilderUtils;
+import Project.model.*;
+import Project.view.ViewablePlane;
+
+public class GenCircleIntersectionBuilder {
+    private GeometricGenCircle a = null;
+    private GeometricGenCircle b = null;
+
+    public boolean acceptArgument(GeometricShape shape) {
+        if (shape instanceof GeometricGenCircle g) {
+            if (a == null) {
+                a = g;
+                return true;
+            }
+            else if (g != a) {
+                b = g;
+                return true;
+            }
+        }
+        if(shape instanceof GeometricCircle c)
+            return acceptArgument(new GeometricGenCircle(c));
+        if(shape instanceof GeometricLine l)
+            return acceptArgument(new GeometricGenCircle(l));
+        return false;
+    }
+
+    public boolean isReady() {
+        return a != null && b != null;
+    }
+
+    public void reset() {
+        a = null;
+        b = null;
+    }
+
+    public void build(ViewablePlane viewablePlane, double planeX, double planeY) {
+        GeometricPoint i1 = new GeometricPoint("Intersection 1");
+        GeometricPoint i2 = new GeometricPoint("Intersection 2");
+        GeometricShapeUpdater updater = new GeometricShapeUpdater() {
+            private final GeometricGenCircle gA = a;
+            private final GeometricGenCircle gB = b;
+
+            @Override
+            public void update() {
+                if(!gA.isLine()) {
+                    if(!gB.isLine())
+                        CircleIntersectionBuilder.setPoints(i1, i2, gA.circle, gB.circle);
+                    else
+                        LineAndCircleIntersectionBuilder.setPoints(i1, i2, gB.line, gA.circle);
+                } else {
+                    if(!gB.isLine())
+                        LineAndCircleIntersectionBuilder.setPoints(i1, i2, gA.line, gB.circle);
+                    else {
+                        LineIntersectionBuilder.setPoint(i1, gA.line, gB.line);
+                        LineIntersectionBuilder.setPoint(i2, gA.line, gB.line);
+                    }
+                }
+            }
+        };
+        i1.setUpdater(updater);
+        i2.setUpdater(updater);
+        BuilderUtils.addToPlane(i1, viewablePlane);
+        BuilderUtils.addToPlane(i2, viewablePlane);
+    }
+}
