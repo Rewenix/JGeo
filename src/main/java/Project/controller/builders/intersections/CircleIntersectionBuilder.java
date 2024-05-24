d9872b78eca19af8274c86e2998c2ab9cc21d1f7
Mateusz Przebieracz <m.przebieracz@student.uj.edu.pl>
2024-05-24 23:05:29 +0200

    rollback

--- src/main/java/Project/controller/builders/intersections/CircleIntersectionBuilder.java
+++ src/main/java/Project/controller/builders/intersections/CircleIntersectionBuilder.java
@@ -2,7 +2,6 @@ package Project.controller.builders.intersections;
 
 import Project.controller.GeometricShapeBuilder;
 import Project.controller.builders.BuilderUtils;
-import Project.controller.builders.lines.LineThroughPointsBuilder;
 import Project.model.*;
 import Project.view.ViewablePlane;
 
@@ -92,7 +91,6 @@ public class CircleIntersectionBuilder implements GeometricShapeBuilder {
         double y3 = y1 + aa * (y2 - y1) / d;
         BasicPoint i1 = new BasicPoint(x3 + h * (y2 - y1) / d, y3 - h * (x2 - x1) / d);
         BasicPoint i2 = new BasicPoint(x3 - h * (y2 - y1) / d, y3 + h * (x2 - x1) / d);
-        BasicLine radicalAxis = LineThroughPointsBuilder.getLine(i1, i2);
-        return LineAndCircleIntersectionBuilder.getPoints(radicalAxis, c1);
+        return List.of(i1, i2);
     }
 }
